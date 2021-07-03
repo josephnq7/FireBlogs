230666bb612cd107963de8371b8d39b11eb4e203
Joseph Nguyen <joseph.nguyen@soaren-management.com>
2021-07-03 16:11:37 -0700

    Blog Card component

--- src/App.vue
+++ src/App.vue
@@ -9,9 +9,8 @@
 </template>
 
 <script>
-
-import Navigation from './components/Navigation';
-import Footer from './components/Footer';
+import Navigation from "./components/Navigation";
+import Footer from "./components/Footer";
 
 export default {
   name: "app",
@@ -72,6 +71,34 @@ export default {
 .arrow-light {
   path {
     fill: #fff;
+  }
+}
+
+.blog-card-wrap {
+  position: relative;
+  padding: 80px 16px;
+  background-color: #f1f1f1;
+
+  @media (min-width: 500px) {
+    padding: 100px 16px;
+  }
+
+  .blog-cards {
+    display: grid;
+    gap: 32px;
+    grid-template-columns: 1fr;
+
+    @media (min-width: 500px) {
+      grid-template-columns: repeat(2, 1fr);
+    }
+
+    @media (min-width: 900px) {
+      grid-template-columns: repeat(3, 1fr);
+    }
+
+    @media (min-width: 1200px) {
+      grid-template-columns: repeat(4, 1fr);
+    }
   }
 }
 </style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,27 +1,43 @@
 <template>
   <div class="home">
     <BlogPost :post="welcomeScreen" />
-    <BlogPost :post="post" v-for="(post, index) in sampleBlogPost" :key="index"/>
+    <BlogPost
+      :post="post"
+      v-for="(post, index) in sampleBlogPost"
+      :key="index"
+    />
+    <div class="blog-card-wrap">
+      <div class="container">
+        <h3>View More Recent Blogs</h3>
+        <div class="blog-cards">
+          <BlogCard :post="post" v-for="(post, index) in sampleBlogCards" :key="index" />
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
 import BlogPost from "../components/BlogPost";
+import BlogCard from "../components/BlogCard";
+
 export default {
   name: "Home",
-  components: {BlogPost},
+  components: { BlogPost, BlogCard },
   data() {
     return {
       welcomeScreen: {
         title: "Welcome!",
-        blogPost: "weekly blog articles with all things programming including HTML, CSS, Javascript and more awesome weekly blog articles with all things programming including HTML, CSS, Javascript and more awesome weekly blog articles with all things programming including HTML, CSS, Javascript and more awesome",
+        blogPost:
+          "weekly blog articles with all things programming including HTML, CSS, Javascript and more awesome weekly blog articles with all things programming including HTML, CSS, Javascript and more awesome weekly blog articles with all things programming including HTML, CSS, Javascript and more awesome",
         welcomeScreen: true,
         photo: "coding",
       },
       sampleBlogPost: [
         {
           title: "This is a Filler Title",
-          blogHTML: "This is a filler blog post title! weekly blog articles with all things programming including HTML, CSS, Javascript and more awesome weekly blog articles with all things programming including HTML, CSS, Javascript and more awesome weekly blog articles with all things programming including HTML, CSS, Javascript and more awesome",
+          blogHTML:
+            "This is a filler blog post title! weekly blog articles with all things programming including HTML, CSS, Javascript and more awesome weekly blog articles with all things programming including HTML, CSS, Javascript and more awesome weekly blog articles with all things programming including HTML, CSS, Javascript and more awesome",
           blogCoverPhoto: "beautiful-stories",
         },
         {
@@ -31,12 +47,37 @@ export default {
         },
       ],
       sampleBlogCards: [
-        {blogTitle: "Blog Card #1", blogCoverPhoto: "stock-1", blogDate: "May 1, 2021"},
-        {blogTitle: "Blog Card #2", blogCoverPhoto: "stock-2", blogDate: "May 1, 2021"},
-        {blogTitle: "Blog Card #3", blogCoverPhoto: "stock-3", blogDate: "May 1, 2021"},
-        {blogTitle: "Blog Card #4", blogCoverPhoto: "stock-4", blogDate: "May 1, 2021"},
-      ]
+        {
+          blogTitle: "Blog Card #1",
+          blogCoverPhoto: "stock-1",
+          blogDate: "May 1, 2021",
+        },
+        {
+          blogTitle: "Blog Card #2",
+          blogCoverPhoto: "stock-2",
+          blogDate: "May 1, 2021",
+        },
+        {
+          blogTitle: "Blog Card #3",
+          blogCoverPhoto: "stock-3",
+          blogDate: "May 1, 2021",
+        },
+        {
+          blogTitle: "Blog Card #4",
+          blogCoverPhoto: "stock-4",
+          blogDate: "May 1, 2021",
+        },
+      ],
     };
   },
 };
 </script>
+<style lang="scss" scoped>
+.blog-card-wrap {
+  h3 {
+    font-weight: 300;
+    font-size: 28px;
+    margin-bottom: 32px;
+  }
+}
+</style>
